d006cde8c025b924965e0894e43e52c0c9383ff3
Dh2013 <t.tajouridhia@gmail.com>
2022-02-11 01:45:13 -0800

    fixing if condition

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,11 +7,11 @@
  */
 int _isupper(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 97 && c <= 122)
 	{
 		return (0);
 	}
-	else if (c >= 'A' && c <= 'Z')
+	else if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
