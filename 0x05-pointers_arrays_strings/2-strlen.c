972f5ce754e8dbb87eb06e55a6edb1c092246a95
Dh2013 <t.tajouridhia@gmail.com>
2022-02-14 08:50:46 -0800

    str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i	=	0;
+
+	while (*(s+i)!= '\0')
+	{ 
+		i++;
+		
+		
+	}
+	return (i);
+}
