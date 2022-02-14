1b9753f83278656eca197d244282fb47fce8f9c8
Dh2013 <t.tajouridhia@gmail.com>
2022-02-14 06:36:39 -0800

    reset1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code 
+ *  reset_to_98 - parameter and updates the value it points t
  *
- * Return: Always 0.
+ * .
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
 
-    return (0);
 }
