df9fadb0661fc0133722980d528719ba51a1744d
Dh2013 <t.tajouridhia@gmail.com>
2022-02-15 05:35:49 -0800

    str

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+ #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */ 
+ char *_strcpy(char *dest, char *src)
+{ 
+	int i;
+	for(i=0; src != "/0" ; i++)
+	{  
+		dest[i] = src[i];
+
+}
+dest[i]= "\0";
+return (dest); 
+}
