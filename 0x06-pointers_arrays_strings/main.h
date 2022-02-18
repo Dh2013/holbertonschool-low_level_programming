d717996a30a0f28d238c8d905f9128b8cd1508bf
Dh2013 <t.tajouridhia@gmail.com>
2022-02-18 01:52:21 -0800

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,5 @@ char *string_toupper(char *);
 void reverse_array(int *a, int n);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
-
+char *_strcat(char *dest, char *src);
 #endif
