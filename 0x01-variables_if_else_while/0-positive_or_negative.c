8a946d713f217a04a9b788d5b61895ab9e0fb668
Dh2013 <t.tajouridhia@gmail.com>
2022-02-22 00:42:06 -0800

    posfix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - function that compaire positive and negative numbers
+ *n: int
+ * Return:0 Success
  **/
 int main(void)
 {
